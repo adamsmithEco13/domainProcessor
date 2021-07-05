90181339b9e65bd7ff66ea06b721884f1677601b
GH2707 <GH2707@surf.com>
2021-07-05 13:29:20 +0300

    spring elastic

--- src/main/java/com/techprimers/domainprocessor/DomainKafkaProcessor.java
+++ src/main/java/com/techprimers/domainprocessor/DomainKafkaProcessor.java
@@ -1,16 +1,18 @@
 package com.techprimers.domainprocessor;
 
 import org.apache.kafka.streams.kstream.KStream;
+import org.elasticsearch.action.get.GetRequest;
+import org.elasticsearch.action.get.GetResponse;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.action.support.WriteRequest.RefreshPolicy;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestHighLevelClient;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.function.cloudevent.CloudEventMessageBuilder;
 import org.springframework.cloud.stream.function.StreamBridge;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.Message;
-
-import java.io.FileWriter;
 import java.io.IOException;
-import java.util.function.Consumer;
 import java.util.function.Function;
 
 @Configuration
@@ -19,11 +21,28 @@ public class DomainKafkaProcessor {
 	@Autowired
 	StreamBridge streamBridge;
 
+	@Autowired
+	RestHighLevelClient highLevelClient;
+
 	@Bean
 	public Function<KStream<String, String>, KStream<String, String>> domainProcessor() {
 		return kstream -> kstream.mapValues(value -> {
+			this.get();
 			return value.toUpperCase();
 		});
 	}
+	
+	public void get() {
+		GetRequest  request = new GetRequest ("dev").id("1");
+		GetResponse response = null;
+		try {
+			response = highLevelClient.get(request, RequestOptions.DEFAULT);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		System.out.println(response);
+		
+	}
+
 
 }
